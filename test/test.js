e222accb9b97ff2c276ac7b099b85cb78ed5f794
Xavier <xavluiz@gmail.com>
2019-04-05 19:33:28 -0700

    updated test case functionality

--- test/test.js
+++ test/test.js
@@ -2,36 +2,55 @@ const expect = require("chai").expect;
 const index = require("../dist/index.js");
 const util = require("../dist/lib/util.js");
 
-describe("get active music player function test", () => {
-    beforeEach(async () => {
-        await index.startSpotifyIfNotRunning();
-        await index.startItunesIfNotRunning();
-        util.sleep(2000);
-    });
+describe("software music tests", () => {
+    beforeEach(async () => {});
 
     after(async () => {
         // await index.stopSpotifyIfRunning();
     });
 
+    // example result
+    // {"artist": "Coldplay","album": "Parachutes","genre": "","disc_number": 1,"duration": 273426,"played_count": 0,"track_number": 6,"id": "spotify:track:0R8P9KfGJCDULmlEoBagcO","name": "Trouble","state":"playing"}
     it("expect to get the running and playing state for spotify", () => {
-        let params = [
-            "spotify:track:0R8P9KfGJCDULmlEoBagcO",
-            "spotify:album:6ZG5lRT77aJ3btmArcykra"
-        ];
+        index.startSpotifyIfNotRunning().then(() => {
+            util.sleep(2000);
+            let params = [
+                "spotify:track:0R8P9KfGJCDULmlEoBagcO",
+                "spotify:album:6ZG5lRT77aJ3btmArcykra"
+            ];
 
-        index.playTrackInContext("Spotify", params).then(() => {
-            index.getState("Spotify").then(result => {
-                console.log("spotify playing result: ", result);
-                expect(true).to.equal(true);
+            index.playTrackInContext("Spotify", params).then(() => {
+                util.sleep(2000);
+                index.getState("Spotify").then(result => {
+                    try {
+                        let obj = JSON.parse(result);
+                        expect(obj.artist).to.equal("Coldplay");
+                    } catch (e) {
+                        console.log("use case error: ", e.message);
+                    } finally {
+                        index.stopSpotifyIfRunning();
+                    }
+                });
             });
         });
     });
 
+    // example result
+    // {"artist": "Loud Forest","album": "Out of Sight - Single","genre":"Alternative","disc_number": 1,"duration": 212.042007446289,"played_count": 120,"track_number": 1,"id": "5601","name": "Out of Sight","state":"playing"}
     it("expect to get the running and playing state for itunes", () => {
-        index.play("iTunes").then(() => {
-            index.getState("iTunes").then(result => {
-                console.log("itunes playing result: ", result);
-                expect(true).to.equal(true);
+        index.startItunesIfNotRunning().then(() => {
+            util.sleep(2000);
+            index.play("iTunes").then(() => {
+                index.getState("iTunes").then(result => {
+                    try {
+                        let obj = JSON.parse(result);
+                        expect(obj.artist).to.not.equal(null);
+                    } catch (e) {
+                        console.log("use case error: ", e.message);
+                    } finally {
+                        index.stopItunesIfRunning();
+                    }
+                });
             });
         });
     });
