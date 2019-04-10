21f17d3cc4fd4f564eaedd9d7e491b3f456738d4
Rick Rickards <rick@software.com>
2019-04-10 11:39:44 -0700

    update project add ES6 .babelrc

--- lib/controller.ts
+++ lib/controller.ts
@@ -1,193 +1,190 @@
-import { execCmd, getPlayerName } from "./util";
-const util = require("util");
+import { execCmd, getPlayerName } from './util'
+import util from 'util'
 
 export class MusicController {
-    private scriptsPath: string = __dirname + "/scripts/";
-    private lastVolumeLevel: any = null;
+  private scriptsPath: string = __dirname + '/scripts/'
+  private lastVolumeLevel: any = null
 
-    // applscript music commands and scripts
-    private scripts: any = {
-        state: {
-            file: "get_state.%s.applescript"
-        },
-        volumeUp: {
-            file: "volume_up.%s.applescript"
-        },
-        volumeDown: {
-            file: "volume_down.%s.applescript"
-        },
-        playTrackInContext:
-            'tell application "%s" to play track "%s" in context "%s"',
-        play: 'tell application "%s" to play',
-        playTrack: 'tell application "%s" to play track %s',
-        pause: 'tell application "%s" to pause',
-        playPause: 'tell application "%s" to playpause',
-        next: 'tell application "%s" to play (next track)',
-        previous: 'tell application "%s" to play (previous track)',
-        repeatOn: 'tell application "%s" to set %s to %s',
-        repeatOff: 'tell application "%s" to set %s to %s',
-        isRepeating: 'tell application "%s" to return %s',
-        setVolume: 'tell application "%s" to set sound volume to %s',
-        mute: 'tell application "%s" to set sound volume to 0',
-        unMute: 'tell application "%s" to set sound volume to %s',
-        setShuffling: 'tell application "%s" to set %s to %s',
-        isShuffling: 'tell application "%s" to %s'
-    };
+  // applscript music commands and scripts
+  private scripts: any = {
+    state: {
+      file: 'get_state.%s.applescript'
+    },
+    volumeUp: {
+      file: 'volume_up.%s.applescript'
+    },
+    volumeDown: {
+      file: 'volume_down.%s.applescript'
+    },
+    playTrackInContext:
+      'tell application "%s" to play track "%s" in context "%s"',
+    play: 'tell application "%s" to play',
+    playTrack: 'tell application "%s" to play track %s',
+    pause: 'tell application "%s" to pause',
+    playPause: 'tell application "%s" to playpause',
+    next: 'tell application "%s" to play (next track)',
+    previous: 'tell application "%s" to play (previous track)',
+    repeatOn: 'tell application "%s" to set %s to %s',
+    repeatOff: 'tell application "%s" to set %s to %s',
+    isRepeating: 'tell application "%s" to return %s',
+    setVolume: 'tell application "%s" to set sound volume to %s',
+    mute: 'tell application "%s" to set sound volume to 0',
+    unMute: 'tell application "%s" to set sound volume to %s',
+    setShuffling: 'tell application "%s" to set %s to %s',
+    isShuffling: 'tell application "%s" to %s'
+  }
 
-    async isMusicPlayerActive(player: string) {
-        player = getPlayerName(player);
-        const command = `pgrep -x ${player}`;
-        // this returns the PID of the requested player
-        const result = await execCmd(command);
-        if (result && !result.error) {
-            return true;
-        }
-        return false;
+  async isMusicPlayerActive(player: string) {
+    player = getPlayerName(player)
+    const command = `pgrep -x ${player}`
+    // this returns the PID of the requested player
+    const result = await execCmd(command)
+    if (result && !result.error) {
+      return true
+    }
+    return false
+  }
+
+  async stopPlayer(player: string) {
+    player = getPlayerName(player)
+    const command = `pgrep -x ${player} | xargs kill -9`
+    let result = await execCmd(command)
+    if (result === null || result === undefined) {
+      result = 'ok'
+    }
+    return result
+  }
+
+  async startPlayer(player: string) {
+    player = getPlayerName(player)
+    const command = `open -a ${player}`
+    let result = await execCmd(command)
+    if (result === null || result === undefined) {
+      result = 'ok'
+    }
+    return result
+  }
+
+  async execScript(player: string, scriptName: string, params: any = null) {
+    player = getPlayerName(player)
+    let script = this.scripts[scriptName]
+
+    if (!params) {
+      // set player to the params
+      params = [player]
+    } else {
+      // push the player to the front of the params array
+      params.unshift(player)
     }
 
-    async stopPlayer(player: string) {
-        player = getPlayerName(player);
-        const command = `pgrep -x ${player} | xargs kill -9`;
-        let result = await execCmd(command);
+    let command = ''
+    // get the script file if the attribut has one
+    if (script.file) {
+      // apply the params (which should only have the player name)
+      const scriptFile = util.format.apply(util, [script.file].concat(params))
+      let file = `${this.scriptsPath}${scriptFile}`
+      command = `osascript ${file}`
+      // script = fs.readFileSync(file).toString();
+    } else {
+      // apply the params to the one line script
+      script = util.format.apply(util, [script].concat(params))
+      // console.log("script to use: ", script);
+      command = `osascript -e \'${script}\'`
+    }
+    let result = await execCmd(command)
+    if (result === null || result === undefined) {
+      result = 'ok'
+    }
+    return result
+  }
+
+  playTrack(player: string, trackId: string) {
+    let params = null
+    if (player === 'Spotify') {
+      params = [`"${trackId}"`]
+    } else {
+      params = [`${trackId}`]
+    }
+    return this.execScript(player, 'playTrack', params).then(result => {
+      if (result === null || result === undefined) {
+        result = 'ok'
+      }
+      return result
+    })
+  }
+
+  async run(player: string, scriptName: string, params: any = null) {
+    if (player === 'Spotify') {
+      if (scriptName === 'repeatOn') {
+        params = ['repeating', 'true']
+      } else if (scriptName === 'repeatOff') {
+        params = ['repeating', 'false']
+      } else if (scriptName === 'isRepeating') {
+        params = ['repeating']
+      } else if (scriptName === 'setShuffling') {
+        // this will already have params
+        params.unshift('shuffling')
+      } else if (scriptName === 'isShuffling') {
+        params = ['return shuffling']
+      }
+    } else if (player === 'iTunes') {
+      if (scriptName === 'repeatOn') {
+        // repeat one for itunes
+        params = ['song repeat', 'one']
+      } else if (scriptName === 'repeatOff') {
+        // repeat off for itunes
+        params = ['song repeat', 'off']
+      } else if (scriptName === 'isRepeating') {
+        // get the song repeat value
+        params = ['song repeat']
+      } else if (scriptName === 'setShuffling') {
+        params.unshift('shuffle enabled')
+      } else if (scriptName === 'isShuffling') {
+        params = ['get shuffle enabled']
+      }
+    }
+
+    if (scriptName === 'mute') {
+      // get the current volume state
+      let stateResult = await this.execScript(player, 'state')
+      let json = JSON.parse(stateResult)
+      this.lastVolumeLevel = json.volume
+    } else if (scriptName === 'unMute') {
+      params = [this.lastVolumeLevel]
+    } else if (scriptName === 'next' || scriptName === 'previous') {
+      // make sure it's not on repeat
+      if (player === 'Spotify') {
+        await this.execScript(player, 'state', ['repeating', 'false'])
+      } else {
+        await this.execScript(player, 'state', ['song repeat', 'off'])
+      }
+    }
+    return this.execScript(player, scriptName, params).then(result => {
+      if (result === null || result === undefined) {
+        result = 'ok'
+      }
+      return result
+    })
+  }
+
+  setVolume(player: string, volume: number) {
+    this.lastVolumeLevel = volume
+    return this.execScript(player, 'setVolume', [volume]).then(result => {
+      if (result === null || result === undefined) {
+        result = 'ok'
+      }
+      return result
+    })
+  }
+
+  playTrackInContext(player: string, params: any[]) {
+    return this.execScript(player, 'playTrackInContext', params).then(
+      result => {
         if (result === null || result === undefined) {
-            result = "ok";
+          result = 'ok'
         }
-        return result;
-    }
-
-    async startPlayer(player: string) {
-        player = getPlayerName(player);
-        const command = `open -a ${player}`;
-        let result = await execCmd(command);
-        if (result === null || result === undefined) {
-            result = "ok";
-        }
-        return result;
-    }
-
-    async execScript(player: string, scriptName: string, params: any = null) {
-        player = getPlayerName(player);
-        let script = this.scripts[scriptName];
-
-        if (!params) {
-            // set player to the params
-            params = [player];
-        } else {
-            // push the player to the front of the params array
-            params.unshift(player);
-        }
-
-        let command = "";
-        // get the script file if the attribut has one
-        if (script.file) {
-            // apply the params (which should only have the player name)
-            const scriptFile = util.format.apply(
-                util,
-                [script.file].concat(params)
-            );
-            let file = `${this.scriptsPath}${scriptFile}`;
-            command = `osascript ${file}`;
-            // script = fs.readFileSync(file).toString();
-        } else {
-            // apply the params to the one line script
-            script = util.format.apply(util, [script].concat(params));
-            // console.log("script to use: ", script);
-            command = `osascript -e \'${script}\'`;
-        }
-        let result = await execCmd(command);
-        if (result === null || result === undefined) {
-            result = "ok";
-        }
-        return result;
-    }
-
-    playTrack(player: string, trackId: string) {
-        let params = null;
-        if (player === "Spotify") {
-            params = [`"${trackId}"`];
-        } else {
-            params = [`${trackId}`];
-        }
-        return this.execScript(player, "playTrack", params).then(result => {
-            if (result === null || result === undefined) {
-                result = "ok";
-            }
-            return result;
-        });
-    }
-
-    async run(player: string, scriptName: string, params: any = null) {
-        if (player === "Spotify") {
-            if (scriptName === "repeatOn") {
-                params = ["repeating", "true"];
-            } else if (scriptName === "repeatOff") {
-                params = ["repeating", "false"];
-            } else if (scriptName === "isRepeating") {
-                params = ["repeating"];
-            } else if (scriptName === "setShuffling") {
-                // this will already have params
-                params.unshift("shuffling");
-            } else if (scriptName === "isShuffling") {
-                params = ["return shuffling"];
-            }
-        } else if (player === "iTunes") {
-            if (scriptName === "repeatOn") {
-                // repeat one for itunes
-                params = ["song repeat", "one"];
-            } else if (scriptName === "repeatOff") {
-                // repeat off for itunes
-                params = ["song repeat", "off"];
-            } else if (scriptName === "isRepeating") {
-                // get the song repeat value
-                params = ["song repeat"];
-            } else if (scriptName === "setShuffling") {
-                params.unshift("shuffle enabled");
-            } else if (scriptName === "isShuffling") {
-                params = ["get shuffle enabled"];
-            }
-        }
-
-        if (scriptName === "mute") {
-            // get the current volume state
-            let stateResult = await this.execScript(player, "state");
-            let json = JSON.parse(stateResult);
-            this.lastVolumeLevel = json.volume;
-        } else if (scriptName === "unMute") {
-            params = [this.lastVolumeLevel];
-        } else if (scriptName === "next" || scriptName === "previous") {
-            // make sure it's not on repeat
-            if (player === "Spotify") {
-                await this.execScript(player, "state", ["repeating", "false"]);
-            } else {
-                await this.execScript(player, "state", ["song repeat", "off"]);
-            }
-        }
-        return this.execScript(player, scriptName, params).then(result => {
-            if (result === null || result === undefined) {
-                result = "ok";
-            }
-            return result;
-        });
-    }
-
-    setVolume(player: string, volume: number) {
-        this.lastVolumeLevel = volume;
-        return this.execScript(player, "setVolume", [volume]).then(result => {
-            if (result === null || result === undefined) {
-                result = "ok";
-            }
-            return result;
-        });
-    }
-
-    playTrackInContext(player: string, params: any[]) {
-        return this.execScript(player, "playTrackInContext", params).then(
-            result => {
-                if (result === null || result === undefined) {
-                    result = "ok";
-                }
-                return result;
-            }
-        );
-    }
+        return result
+      }
+    )
+  }
 }
--- lib/util.ts
+++ lib/util.ts
@@ -1,75 +1,70 @@
-const { exec } = require("child_process");
+import { exec } from 'child_process'
 
-export const SPOTIFY_NAME = "Spotify";
-export const ITUNES_NAME = "iTunes";
+export const SPOTIFY_NAME = 'Spotify'
+export const ITUNES_NAME = 'iTunes'
 
 export function isLinux() {
-    return isWindows() || isMac() ? false : true;
+  return isWindows() || isMac() ? false : true
 }
 
 // process.platform return the following...
 //   -> 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
 export function isWindows() {
-    return process.platform.indexOf("win32") !== -1;
+  return process.platform.indexOf('win32') !== -1
 }
 
 export function isMac() {
-    return process.platform.indexOf("darwin") !== -1;
+  return process.platform.indexOf('darwin') !== -1
 }
 
 export function isBooleanString(val: string) {
-    if (
-        (val && val.toLowerCase() === "true") ||
-        val.toLowerCase() === "false"
-    ) {
-        return true;
-    }
-    return false;
+  if ((val && val.toLowerCase() === 'true') || val.toLowerCase() === 'false') {
+    return true
+  }
+  return false
 }
 
 export async function execCmd(cmd: string, projectDir: any = null) {
-    let result: any = null;
-    try {
-        let opts =
-            projectDir !== undefined && projectDir !== null
-                ? { cwd: projectDir }
-                : {};
-        result = await execPromise(cmd, opts);
-    } catch (e) {
-        result = { error: e.message };
-    }
-    return result;
+  let result: any = null
+  try {
+    let opts =
+      projectDir !== undefined && projectDir !== null ? { cwd: projectDir } : {}
+    result = await execPromise(cmd, opts)
+  } catch (e) {
+    result = { error: e.message }
+  }
+  return result
 }
 
 async function execPromise(command: string, opts: {}) {
-    return new Promise((resolve, reject) => {
-        exec(command, opts, (error: any, stdout: string, stderr: any) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-            resolve(stdout.trim());
-        });
-    });
+  return new Promise((resolve, reject) => {
+    exec(command, opts, (error: any, stdout: string, stderr: any) => {
+      if (error) {
+        reject(error)
+        return
+      }
+      resolve(stdout.trim())
+    })
+  })
 }
 
 // Sleep for the designated milliseconds.
 // It should not be used in lib but only in the test.
 // It has a max of 5 seconds as this is resource intensive
 export function sleep(delayInMillis: number) {
-    delayInMillis = Math.min(delayInMillis, 5000);
-    var start = new Date().getTime();
-    while (new Date().getTime() < start + delayInMillis);
+  delayInMillis = Math.min(delayInMillis, 5000)
+  var start = new Date().getTime()
+  while (new Date().getTime() < start + delayInMillis);
 }
 
 export function getPlayerName(player: string) {
-    if (!player || player.trim().length === 0) {
-        player = "Spotify";
-        return player;
-    }
-    player = player.trim().toLowerCase();
-    if (player === "itunes") {
-        return ITUNES_NAME;
-    }
-    return SPOTIFY_NAME;
+  if (!player || player.trim().length === 0) {
+    player = 'Spotify'
+    return player
+  }
+  player = player.trim().toLowerCase()
+  if (player === 'itunes') {
+    return ITUNES_NAME
+  }
+  return SPOTIFY_NAME
 }
