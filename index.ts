c57a30dee77c1af5813d20dc71f5908822a3eea5
Xavier <xavluiz@gmail.com>
2019-05-21 23:07:38 -0700

    added remainder models to index

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 "use strict";
 
 export * from "./lib/apis";
-export { PlayerName, PlayerDevice, SpotifyAudioFeature } from "./lib/models";
+export {
+    PlayerName,
+    Track,
+    TrackStatus,
+    PlayerDevice,
+    PlayerContext,
+    SpotifyAudioFeature,
+    PlayerType
+} from "./lib/models";
